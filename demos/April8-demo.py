44bccbde483babb7e8a94d96c4821e1dd40fbf37
Olga Zamaraeva <olzama@uw.edu>
2021-04-08 12:48:30 -0700

    no message

---
+++ demos/April8-demo.py
@@ -0,0 +1,36 @@
+# This is a comment. This line won't be executed.
+# Demo for April 8.
+
+# Statements. Output.
+print("Hello world")
+
+
+# Different interpreters
+
+# Variables. Assignment.
+x = 5
+y = 0
+z = x + y
+
+# Printing out non-strings. Types. "Dynamic typing". Math operators.
+print(z)
+print(x+y)
+
+# Illegal operations, broken programs.
+
+# print(x/y) # Uncomment this to see the program break!
+
+print("hello again")
+
+# Functions
+
+
+def computeSum(a, b):
+    c = a + b
+    return c
+
+
+result1 = computeSum(1, 2)
+result2 = computeSum(x, y)
+
+print("End")
