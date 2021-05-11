fe68432a64ad5c30376da03eeabcc08e500e8d87
Olga Zamaraeva <olzama@uw.edu>
2021-05-10 17:22:34 -0700

    no message

---
+++ demos/May11-skeleton.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+
+
+# The data: Distance needed for cars at different speeds to stop
+data_url = 'https://raw.githubusercontent.com/cmdlinetips/data/master/cars.tsv'
