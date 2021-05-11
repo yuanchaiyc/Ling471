64c24a6a71015cbd972ccc6bd205b1cc2107e4ba
Olga Zamaraeva <olzama@uw.edu>
2021-05-10 17:29:07 -0700

    no message

--- demos/May11-skeleton.py
+++ demos/May11-skeleton.py
@@ -4,6 +4,25 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
 
+# Matrix multiplication:
+# Data
+
+matrix1 = [(1, 1, 2),
+
+           (0, 2, 1),
+
+           (2, 0, 1)]
+
+ 
+
+matrix2 = [(2, 0, 2),
+
+           (1, 1, 1),
+
+           (2, 2, 2)]
+
+
+
 
 # The data: Distance needed for cars at different speeds to stop
 data_url = 'https://raw.githubusercontent.com/cmdlinetips/data/master/cars.tsv'
