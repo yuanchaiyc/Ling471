a65a5f4e6731922e069d8589f4d453df6dfc0ad4
Olga Zamaraeva <olzama@uw.edu>
2021-05-27 10:00:48 -0700

    Update May27-activity-plots.py

--- assignments/May27-activity-plots.py
+++ assignments/May27-activity-plots.py
@@ -8,7 +8,7 @@ Y = [pow(x, 2) for x in X]
 plt.plot(X, Y) plt.show()
 plt.xlabel('X', color='fuchsia', fontweight='bold')
 plt.ylabel('f(x)')
-plt.plot(X, Y, 'o-', label='squared')
+plt.plot(X, Y, 'o-', label='squared') # other styles: '--', ':'
 plt.legend(loc="upper left")
 
 plt.savefig('figures/parabola.png')
