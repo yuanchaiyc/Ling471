2bab80280bbbd1d399096c755c922d1c3e53cd4a
Matthew C. Kelley <matthew.curtis.kelley@gmail.com>
2023-04-04 12:12:50 -0700

    Add 4-4 demo

---
+++ demos/4_4/printing_demo.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+
+    print('Matthew C. Kelley')
+    print()
+    print('This is a sentence.')
+    print()
+    print('*'*4)
+    print('*' + '  ' + '*')
+    print('*' + '  ' + '*')
+    print('*'*4)
+    print()
+    print(' # ')
+    print('# #')
+    print('###')
+    print('# #')
+	
