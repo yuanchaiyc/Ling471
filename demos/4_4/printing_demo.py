388cb5e45df7379d97b2a79cd2a6089226d65025
Matthew C. Kelley <matthew.curtis.kelley@gmail.com>
2023-04-05 10:50:27 -0700

    Add comments to demo

--- demos/4_4/printing_demo.py
+++ demos/4_4/printing_demo.py
@@ -1,14 +1,22 @@
 if __name__ == '__main__':
 
+    # Print your name
     print('Matthew C. Kelley')
     print()
+    
+    # Print a sentence
     print('This is a sentence.')
     print()
+    
+    # Print a 4x4 square
     print('*'*4)
     print('*' + '  ' + '*')
     print('*' + '  ' + '*')
     print('*'*4)
     print()
+    
+    # Print some kind of shape with ASCII characters
+    # In this case, the letter "A" made up of "#"
     print(' # ')
     print('# #')
     print('###')
