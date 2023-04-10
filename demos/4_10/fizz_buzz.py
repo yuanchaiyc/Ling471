be25f2068d3bfc1b5bc5781c136781583d3c3096
Matthew C. Kelley <matthew.curtis.kelley@gmail.com>
2023-04-10 11:40:04 -0700

    Add fizzbuzz skeleton

---
+++ demos/4_10/fizz_buzz.py
@@ -0,0 +1,22 @@
+if __name__ == '__main__':
+
+    # Fill in the code to do the following
+    # 1. Set x to be a non-negative integer (no decimals, no negatives)
+    # 2. If x is divisible by 3, print 'Fizz'
+    # 3. If x is divisible by 5, print 'Buzz'
+    # 4. If x is divisible by both 3 and 5, print 'FizzBuzz'
+    # 5. If x is divisible by neither 3 nor 5, do not print anything
+    #
+    # How to check if a number is divisble by another number?
+    # Use modulus division! Modulus division tells you what the
+    # remainder is after you do as many divisions as you can. It is performed
+    # with the % operator.
+    #
+    # For example, 7 / 4 = 1 with 3 remaining. Modulus division will return
+    # 3 for this example, that is, 7 % 4, will return 3. If a number is x is
+    # divisible by another number y, x % y will be 0. So, 4 % 2 = 0.
+    
+    # Change assignment here
+    x = 1
+    
+    # Insert conditional(s) here
