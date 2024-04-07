b0b581b86f74d7dd07f2b8a96ac5ab922a08a9e7
yuanchaiyc <cyjy2017@gmail.com>
2024-04-07 15:24:15 -0700

    add 0402 demo

---
+++ demos/04-02-24/printing_demo.py
@@ -0,0 +1,26 @@
+# Print your name
+print('Yuan Chai')
+print()
+
+# Print a sentence
+print('This is a sentence.')
+print()
+
+# Print a 4x4 square
+print('*'*4)
+print('*' + '  ' + '*')
+print('*' + '  ' + '*')
+print('*'*4)
+print()
+
+# Print some kind of shape with ASCII characters
+# In this case, the letter "A" made up of "#"
+print(' # ')
+print('# #')
+print('###')
+print('# #')
+print()
+
+# Print with multiple lines and tabs
+print('Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\t\tLike a diamond in the sky.')
+
